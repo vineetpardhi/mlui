dede6f7da2e01e2082060bc477e659d32253a3d7
vineetpardhi <51925185+vineetpardhi@users.noreply.github.com>
2020-03-07 18:37:10 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 const routes: Routes = [
 {
-  path: "dashboard",component: DashboardComponent
+  path:'',component:HomeComponent
+  
+},
+{
+  path:"dashboard",component:DashboardComponent
 }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    HomeComponent,
     
   ],
   imports: [
