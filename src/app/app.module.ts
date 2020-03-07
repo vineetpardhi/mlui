2642a12ae96ccfe2194239c76373598f9a078ba8
vineetpardhi <51925185+vineetpardhi@users.noreply.github.com>
2020-03-07 20:58:59 +0530

    ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CollapseComponent, CollapseModule, WavesModule, DropdownModule } from 'angular-bootstrap-md';
+import { MatButtonModule } from '@angular/material/button';
+import { MatList, MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HttpClientModule }    from '@angular/common/http';
 
 
 @NgModule({
@@ -16,7 +27,21 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatExpansionModule,
+    CollapseModule,
+    WavesModule,
+    MatButtonModule,
+    MatListModule,
+    MatInputModule,
+    DropdownModule,
+    MatGridListModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+// For MDB Angular Free
+import { CollapseModule, WavesModule, DropdownModule} from 'angular-bootstrap-md';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  panelOpenState = false;
 
+  foods: Food[] = [
+    {value: 'trans', viewValue: 'Transfer'},
+    {value: 'arch', viewValue: 'Architecture'},
+  ];
   constructor() { }
 
   ngOnInit() {
